2076331987d9f55482b1e64dafc50d877ba264b6
Leology0 <leology42@gmail.com>
2023-09-19 13:19:16 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+# Investment Accounts Assignment
+
+def main():
+    # Create menu loop
+    done = False
+
+    while not done:
+        # Print Menu
+        print ("\nMAIN MENU")
+        print("1: Print Accounts")
+        print("2: Deposit")
+        print("3: Withdrawal")
+        print("4: Count under $2000")
+        print("5: Generous Donor")
+        print("6: Hacker Attack")
+        print("7: Exit")
+
+        # Get selection from user
+        selection = input("\nEnter Option #: ")
+
+
+if __name__ == "__main__":
+    main()
